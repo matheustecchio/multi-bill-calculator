5770484a652481e49f23a9a3f2c29a196d5fce53
Matheus Tecchio <52295230+matheustecchio@users.noreply.github.com>
2024-04-08 17:01:06 +0100

    increase perfomance and rename function (#1)
    * set option variable as int instead of string
    
    * rename function name

--- water_charges.py
+++ water_charges.py
@@ -1,10 +1,10 @@
-def water_charges():
-    yearly_household_consumption = int(input("How many liters did you use this year: "))
+def calculate_charges():
+    yearly_household_consumption = int(input("\nHow many liters did you use this year: "))
     number_of_adults = int(input("Enter with the quantity of adults in our house: "))
     number_of_children = int(input("Enter with the quantity of children in our house: "))
 
 
-    # Calculating the charge free limit and its rules.
+    # Calculating the charge free limit and its rules
     charge_free_limit = 213000 # Standard limit
 
     if number_of_adults >= 5:
@@ -18,7 +18,7 @@ def water_charges():
             charge_free_limit += 7000 * (number_of_children - 2)
 
 
-    # Calculating total charge and its rules.
+    # Calculating total charge and its rules
     charge_rate_litre = 3.70
     total_charge = (yearly_household_consumption - charge_free_limit) * charge_rate_litre
     if total_charge < 0:
@@ -29,4 +29,4 @@ def water_charges():
         f"\nYour Charge is: €{total_charge:,.2f}")
 
 if __name__ == "__main__":
-    water_charges()
+    calculate_charges()
