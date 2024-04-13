dcf16918e1b99f7ac0b936319e7779450c2acd20
Matheus Tecchio <matheusdiastecchio@gmail.com>
2024-04-13 21:24:37 +0100

    add header comment

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
+"""
+Author: Matheus Dias Tecchio
+Data: 06/04/2024
+
+The main code that runs the interactive menu to execute all our functions.
+"""
 import water_charges
 import school_transport
 
